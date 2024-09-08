e04c23bd1db0287d9a66d0a7121299205d460927
Masapex089 <takahashi.89th@gmail.com>
2024-09-08 18:56:00 +0900

    itemモデルとitemコントローラーの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :explanation, :price, :category_id, :item_status_id, :delivery_fee_id, :prefecture_id, :delivery_day_id).merge(user_id: current_user.id) # rubocop:disable Layout/LineLength
   end
 end
