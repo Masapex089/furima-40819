ee1a6bb1976b4d1b543fdd5b6222107df2572dea
Masapex089 <takahashi.89th@gmail.com>
2024-09-14 10:55:50 +0900

    Active Storageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :explanation, :price, :image, :category_id, :item_status_id, :delivery_fee_id, :prefecture_id, :delivery_day_id).merge(user_id: current_user.id) # rubocop:disable Layout/LineLength
+  end
 end
