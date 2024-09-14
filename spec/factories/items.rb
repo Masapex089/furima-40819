16fd7f3f9a0124c8e9ef8313579e04f017ec9e20
Masapex089 <takahashi.89th@gmail.com>
2024-09-14 16:30:16 +0900

    テストコード修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     explanation      { Faker::Lorem.sentence }
     price            { 10_000 }
     category_id      { 2 }
-    item_state_id    { 2 }
+    item_status_id   { 2 }
     delivery_fee_id  { 2 }
     prefecture_id    { 2 }
     delivery_day_id { 2 }
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   before do
-    @item = FactoryBot.build(:item)
+    @items = FactoryBot.build(:item)
   end
 
   describe '商品出品' do
