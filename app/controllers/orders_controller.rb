ff396b3e143a1d4298f1aa5ddcdf4625685db256
Masapex089 <takahashi.89th@gmail.com>
2024-09-17 12:36:12 +0900

    他機能部分の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!, only: :index
   before_action :set_order, only: [:index, :create]
+  before_action :move_to_index, only: :index
   def index
     gon.public_key = ENV['PAYJP_PUBLIC_KEY']
     @order_address = OrderAddress.new
@@ -35,5 +36,9 @@ class OrdersController < ApplicationController
       card: order_params[:token],
       currency: 'jpy'
     )
+  end
+
+  def move_to_index
+    redirect_to root_path if current_user.id == @order.user_id
   end
 end
